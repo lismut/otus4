1dacd265ee79b95891e3f477bf33dd622e72357d
Lavrov Aleksandr <lismut@gmail.com>
2020-04-28 11:50:03 +0300

    Repair CMake

--- testg.cpp
+++ testg.cpp
@@ -1,10 +1,7 @@
 #include <gtest/gtest.h>
+#include <list>
+#include <string>
 #include "lib.h"
-
-TEST(GOOGLE_TEST1, test2)
-{
-	ASSERT_TRUE(version() > 1);
-}
 
 TEST(GOOGLE_TEST2, test3)
 {
@@ -13,30 +10,35 @@ TEST(GOOGLE_TEST2, test3)
 
 TEST(GOOGLE_TEST3, test4)
 {
-    ASSERT_TRUE(print_ip(short{0}) == "0.0.0.0");
+    ASSERT_TRUE(print_view_ip(short{0}) == "0.0.0.0");
 }
 
 TEST(GOOGLE_TEST4, test5)
 {
-    ASSERT_TRUE(print_ip(int{2130706433}) == "127.0.0.1");
+    ASSERT_TRUE(print_view_ip(int{2130706433}) == "127.0.0.1");
 }
 
 TEST(GOOGLE_TEST5, test6)
 {
-    ASSERT_TRUE(print_ip<long long>(8875824491850138409) == "123.45.67.89.101.112.131.41");
+    ASSERT_TRUE(print_view_ip<long long>(8875824491850138409) == "123.45.67.89.101.112.131.41");
 }
 
 TEST(GOOGLE_TEST6, test7)
 {
-    ASSERT_TRUE(print_ip(std::string{"trololo"}) == "trololo");
+    ASSERT_TRUE(print_view_ip(std::string{"trololo"}) == "trololo");
 }
 
 TEST(GOOGLE_TEST7, test8)
 {
-    ASSERT_TRUE(print_ip(std::vector{0, 1, 2, 3}) == "0.1.2.3");
+    ASSERT_TRUE(print_view_ip(std::vector{0, 1, 2, 3}) == "0.1.2.3");
 }
 
 TEST(GOOGLE_TEST8, test9)
 {
-    ASSERT_TRUE(print_ip(std::list{10, 20, 30, 40}) == "10.20.30.40");
+    ASSERT_TRUE(print_view_ip(std::list{10, 20, 30, 40}) == "10.20.30.40");
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
